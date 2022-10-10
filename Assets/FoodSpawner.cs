32cd5f65af3e93256a6930dd129d62a7322eeebb
nojuskk <nojus.krauklys@gmail.com>
2022-10-10 18:50:31 +0300

    Upload

---
+++ Assets/FoodSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameObject.FindGameObjectsWithTag("Food").Length == 0)
+        {
+            double x = Random.Range(borderLeft.position.x, borderRight.position.x);
+            double y = Random.Range(borderBottom.position.y, borderTop.position.y);
+            x = System.Math.Round(x, 1);
+            y = System.Math.Round(y, 1);
+
+            if (x > 0)
+                x -= 0.1;
+            else if (x < 0)
+                x += 0.1;
+            if (y > 0)
+                y -= 0.1;
+            else if(y < 0)
+                y += 0.1;
+
+            Instantiate(foodPrefab, new Vector2((float)x, (float)y), Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/SnakeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class SnakeController : MonoBehaviour {
+
+
+    public Vector2 direction = Vector2.right;
+    public List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    public GameObject tailPrefab;
+    public AudioSource eatSound;
+    
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Move", 0.10f, 0.10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) && direction != Vector2.right)
+            direction = Vector2.left;
+        else if (Input.GetKey(KeyCode.DownArrow) && direction != Vector2.up)
+            direction = Vector2.down;
+        else if (Input.GetKey(KeyCode.RightArrow) && direction != Vector2.left)
+            direction = Vector2.right;
+        else if (Input.GetKey(KeyCode.UpArrow) && direction != Vector2.down)
+            direction = Vector2.up;
+	}
+
+    void Move()
+    {
+        Vector2 GapPos = transform.position;
+        transform.Translate(direction * 0.1f);
+
+        if (ate)
+        {
+            GameObject tailClone = Instantiate(tailPrefab, GapPos, Quaternion.identity);
+            tail.Insert(0, tailClone.transform);
+            ate = false;
+        }
+        else if (tail.Count > 0)
+        {
+            tail.Last().position = GapPos;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            ate = true;
+            Destroy(collision.gameObject);
+            eatSound.Play();
+
+        }
+        else if(collision.gameObject.tag == "Wall")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
